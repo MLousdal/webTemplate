6555d428fc28180feb3bde6b8feec118c098fa18
Mads Lousdal <74186479+MLousdal@users.noreply.github.com>
2021-03-15 23:28:47 +0100

    Added burgerMenu

---
+++ assets/js/scripts.js
@@ -0,0 +1,10 @@
+function myFunction() {
+    const navLinks = document.getElementById("nav-links");
+
+    if (navLinks.style.display === "block") {
+        navLinks.style.display = "none";
+
+    } else {
+        navLinks.style.display = "block";
+    }
+  }
