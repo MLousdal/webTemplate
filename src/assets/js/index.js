5269216d695d3b65d31ec471dcdc5c42a093d995
Mads Lousdal <74186479+MLousdal@users.noreply.github.com>
2021-09-21 22:40:29 +0200

    3.1.1
    Renamed spacing to unit
    Restructured utility classes (flex)
    Added remaining input types to forms
    Table can scroll-x on small screens
    Added loading animation
    More btns, also in showcase
    Added switch module

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,1 +1,13 @@
 // import * as classes from '../scss/main.scss'; // For css in js
+
+// Theme toggle
+const body = document.querySelector("body");
+const themeToggle = document.querySelector("#themeToggle");
+
+themeToggle.addEventListener("click", () => {
+  if (body.classList.contains("theme-light")) {
+    body.classList.replace("theme-light", "theme-dark");
+  } else {
+    body.classList.replace("theme-dark", "theme-light");
+  }
+});
