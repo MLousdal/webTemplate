b5de2fb10f31e0bc5c837cd7300f8ea7591eada4
Mads Lousdal <74186479+MLousdal@users.noreply.github.com>
2021-09-22 14:19:30 +0200

    3.1.3
    auto detect preferred colour scheme

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -2,12 +2,29 @@
 
 // Theme toggle
 const body = document.querySelector("body");
-const themeToggle = document.querySelector("#themeToggle");
+const mqPCS = window.matchMedia("prefers-color-scheme");
 
-themeToggle.addEventListener("click", () => {
-  if (body.classList.contains("theme-light")) {
-    body.classList.replace("theme-light", "theme-dark");
-  } else {
-    body.classList.replace("theme-dark", "theme-light");
+window.onload = () => {
+  const userPrefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+  const userPrefersLight = window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches;
+  
+  if(userPrefersDark){
+    body.classList.add("theme-dark");
   }
-});
+  
+  if(userPrefersLight){
+    body.classList.add("theme-light");
+  }
+  
+}
+
+const themeToggle = document.querySelector("#themeToggle");
+if (themeToggle) {
+  themeToggle.addEventListener("click", () => {
+    if (body.classList.contains("theme-light")) {
+      body.classList.replace("theme-light", "theme-dark");
+    } else {
+      body.classList.replace("theme-dark", "theme-light");
+    }
+  });
+}
